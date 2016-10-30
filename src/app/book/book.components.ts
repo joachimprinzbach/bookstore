a38761dac159335669c2e5685bc276da5cbbae9e
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-10-30 17:09:15 +0100

    Add basic styles for books.

--- src/app/book/book.components.ts
+++ src/app/book/book.components.ts
@@ -4,6 +4,7 @@ import {BookVolume} from '../custom-types/bookVolume';
 @Component({
     selector: 'book',
     templateUrl: './book.component.html',
+    styleUrls: ['./book.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent {
