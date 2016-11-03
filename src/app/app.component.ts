7744303e019136f214359f45c25502f100877609
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-11-03 21:07:30 +0100

    remove unused import.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import {AngularFire} from 'angularfire2';
 import {GoogleBooksService} from './shared/google-books.service';
 import {Observable} from 'rxjs';
 import 'rxjs/Rx';
